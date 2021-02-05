4d4427d8dd62dc38576456eba509d214a9ca764e
LittleRedButton <ritz1484@cs.com>
2021-02-04 17:03:30 -0700

    commit tree da62212d62979ae252ec74d6a28b5aa748d4732f

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require 'pry'
+require 'json'
+file = File.read('spec/fixtures/contestants.json')
+data = JSON.parse(file)
+
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].find do |element|
+    element["status"] == "Winner"
+  end["name"].split.first
 end
+
+
+def all_contestants data
+  data.values.flatten #.flatten removes a layer of the data structure
+end 
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.values.flatten.find do |contestant|
+    contestant["occupation"] == occupation
+  end["name"]
 end
 
+
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.reduce(0) do |sum, contestant|
+    if contestant["hometown"] == hometown
+      sum + 1
+    else
+      sum
+    end
+  end 
 end
 
 def get_occupation(data, hometown)
-  # code here
-end
+  data.values.flatten.find do |contestant|
+    contestant["hometown"] == hometown
+  end["occupation"]
+end 
+
+# def get_average_age_for_season(data, season)
+#   age_total = data[season].sum do |contestant|
+#     contestant["age"].to_f
+#   end
+#   (age_total / data[season].length).round
+# end
+
+#OR...
 
 def get_average_age_for_season(data, season)
-  # code here
-end
+  age_total = data[season].reduce(0) do |sum, contestant|
+    sum + contestant["age"].to_f
+  end
+  (age_total / data[season].length).round
+end 
